59a3b8da0a4306e2649fdd443c265071af58e04c
Oleynik Sophie <Sophie@MacBook-Air-Oleynik.local>
2016-10-29 16:01:33 +0300

    Hello World

--- Test/main.swift
+++ Test/main.swift
@@ -1,12 +1,6 @@
-//
-//  main.swift
-//  Test
-//
-//  Created by Oleynik Sophie on 29.10.16.
-//  Copyright © 2016 Lozoavaia Sofia. All rights reserved.
-//
 
 import Foundation
 
-print("Hello, World!")
+let str = "Hello, World!"
+print(str)
 
