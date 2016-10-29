4cc00152e6375e58f1c52c70ca25a50c54f8ea3c
Oleynik Sophie <Sophie@MacBook-Air-Oleynik.local>
2016-10-29 15:48:20 +0300

    Initial Commit

---
+++ Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Test
+//
+//  Created by Oleynik Sophie on 29.10.16.
+//  Copyright © 2016 Lozoavaia Sofia. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
